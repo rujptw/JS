723f004e5626c344ebee108f08e726130b0f112a
你是不是在做梦 <31233636+rujptw@users.noreply.github.com>
2018-01-13 10:14:47 +0800

    Update Building Strings From a Object.js

--- Building Strings From a Object.js
+++ Building Strings From a Object.js
@@ -1,7 +1,1 @@
-      function solution(pairs) {
-          var str = '';
-          for (var i in pairs) {
-              str += i + " = " + pairs[i] + ",";
-          }
-          return str.slice(0, str.length - 1);
-      }
+
