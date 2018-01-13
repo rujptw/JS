3c9281345e43d5b3dd51999022048dfd2d6a0c11
你是不是在做梦 <31233636+rujptw@users.noreply.github.com>
2018-01-13 15:59:48 +0800

    Update Building Strings From a Object.js

--- codewar/Building Strings From a Object.js
+++ codewar/Building Strings From a Object.js
@@ -1,4 +1,5 @@
-      function solution(pairs) {
+//对象不能直接转化为字符串，需要通过for in 的帮助，如果不这样，会被转换成"[objectobject]"的形式     
+function solution(pairs) {
           var str = '';
           for (var i in pairs) {
               str += i + " = " + pairs[i] + ",";
