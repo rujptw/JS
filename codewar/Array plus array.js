c53160c074f1561b3930aaaed14f487afbae2124
你是不是在做梦 <31233636+rujptw@users.noreply.github.com>
2018-01-15 10:30:24 +0800

    Create Array plus array.js

---
+++ codewar/Array plus array.js
@@ -0,0 +1,7 @@
+    function arrayPlusArray(arr1, arr2) {
+        var sum = 0;
+        for (var i = 0; i < arr1.length; i++) {
+            sum += arr1[i] + arr2[i];
+        }
+        return sum;
+    }
