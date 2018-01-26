27f1235466519b82455c41e9b30b762d47953e33
你是不是在做梦 <31233636+rujptw@users.noreply.github.com>
2018-01-26 10:40:43 +0800

    Create Are You Playing Banjo.js

---
+++ codewar/Are You Playing Banjo.js
@@ -0,0 +1,10 @@
+function areYouPlayingBanjo(name) {
+    if(name[0]==="R"){
+        return name+" plays banjo";
+    }
+    else if(name[0]==="r"){
+        return name+" plays banjo";
+    }else{
+        return name + " does not play banjo";
+    }
+}
